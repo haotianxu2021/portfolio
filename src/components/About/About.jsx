a58195464749192b65605fa5a2a6a016bf48e3d4
haotianxu2021 <horacexu@usc.edu>
2022-08-30 15:17:57 -0700

    change resume and photo

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ import DownloadIcon from '@mui/icons-material/Download';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import ChatIcon from '@mui/icons-material/Chat';
-import photo from '../../assets/photo_Haotian Xu.jpg';
+// import photo from '../../assets/photo_Haotian Xu.jpg';
 import img from '../../assets/Photo_Haotian_Xu_Startinvest.jpg'
 const About = () => {
   return (
